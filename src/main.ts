f1091173bc6896792c65cfbb70ef6759e7386cb8
vicse <vicseore@gmail.com>
2025-01-30 14:01:20 -0500

    feat: change microservice for NATS comunication to create payment session

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './common/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
@@ -17,6 +18,17 @@ async function bootstrap() {
       },
     }),
   );
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+    { inheritAppConfig: true },
+  );
+
+  await app.startAllMicroservices();
   await app.listen(envs.port);
   logger.log(`Payments ms running on port ${envs.port}`);
 }
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -30,7 +30,11 @@ export class PaymentsService {
       cancel_url: envs.stripeCancelUrl,
     });
 
-    return session;
+    return {
+      cancelUrl: session.cancel_url,
+      successUrl: session.success_url,
+      url: session.url,
+    };
   }
 
   stripeWebhook(req: Request, res: Response) {
